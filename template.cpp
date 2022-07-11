decec3f347e3a51ff6c22c259aeba1eabfe3975d
Korn_SAMART <kasettakorn1997@gmail.com>
2022-07-11 13:33:24 +0700

    feat: Initial folder 2022

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define ROF(i,a,b) for(int i=b-1; i>=a; i--)
+#define SORT(a,n) sort(a, a+n)
+#define SORTR(a,n) sort(a, a+n, greater<int>())
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+typedef pair<int, char> pic;
+typedef pair<int, string> pis;
+typedef vector<pair<int,int>> vpii;
+
+void solve(int tc)
+{
+    cout << "Case #" << tc << ": ";
+    
+
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T;
+    cin >> T;
+    for(int i=1; i<=T; i++) {
+        solve(i);
+    }
+    return 0;
+}
